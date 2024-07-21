6443e4113fff64ef2179ce6a075390052e7c3a64
Jiuso <jesusgg200x@gmail.com>
2024-07-22 00:43:39 +0200

    Login working.

--- src/main/java/com/team/indexpulse/controller/WebController.java
+++ src/main/java/com/team/indexpulse/controller/WebController.java
@@ -2,6 +2,7 @@ package com.team.indexpulse.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestClient;
 import com.team.indexpulse.entity.UserAccount;
@@ -16,32 +17,43 @@ public class WebController {
 
     private final RestClient restClient = RestClient.create();
 
-    @PostMapping("/user_accounts")
-    public String postUserAccount(UserAccount userAccount) {
+    @PostMapping("/user_accounts/register")
+    public String postUserAccountRegister(UserAccount userAccount) {
         userAccount.setId(UUID.randomUUID().toString());//We generate the unique ID before posting it.
-
-        //Attribute debugging:
-        System.out.println(userAccount.getId());
-        System.out.println(userAccount.getEmail());
-        System.out.println(userAccount.getPassword());
-        System.out.println(userAccount.getFirstName());
-        System.out.println(userAccount.getLastName());
-
-        System.out.println("Posting user account");
 
         //The POST request is sent to IndexPulseAPI.
         ResponseEntity<Void> response = restClient.post()
-                .uri("http://localhost:7634/user_accounts")
+                .uri("http://localhost:7634/user_accounts/register")
                 .contentType(APPLICATION_JSON)
                 .body(userAccount)
                 .retrieve()
                 .toBodilessEntity();
 
-        if(response.getStatusCode().isError()){
+        if (response.getStatusCode().isError()) {
             System.out.println("Error posting user account");
-        }else{
+        } else {
             System.out.println("User account posted");
         }
+
+        return "test";
+    }
+
+    @PostMapping("/user_accounts/login")
+    public String postUserAccountLogin(UserAccount userAccount, Model model) {
+        Boolean login = false;
+        //The POST request is sent to IndexPulseAPI.
+        ResponseEntity<Boolean> response = restClient.post()
+                .uri("http://localhost:7634/user_accounts/login")
+                .contentType(APPLICATION_JSON)
+                .body(userAccount)
+                .retrieve()
+                .toEntity(Boolean.class);
+
+        login = response.getBody();
+
+        System.out.println("The value received is " + login);
+
+        model.addAttribute("login",login);//The controller sends login variable to the web template (test).
 
         return "test";
     }
